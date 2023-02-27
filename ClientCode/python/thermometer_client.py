8da4091eb002cccaac0ac04e0854725f3138d886
Arthur <arthurmb2001@hotmail.com>
2023-02-27 20:19:37 -0300

    Tirando a autenticação

--- ClientCode/python/thermometer_client.py
+++ ClientCode/python/thermometer_client.py
@@ -8,15 +8,8 @@ import iot_service_pb2_grpc
 
 from const import *
 
-
 def run():
-    credentials = grpc.ssl_channel_credentials()
-    auth_credentials = grpc.metadata_call_credentials(metadata_plugin)
-    composite_credentials = grpc.composite_channel_credentials(credentials, auth_credentials)
-    with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT, composite_credentials) as channel:
-        
-        metadata = (('username1', 'user1'), ('username2','user2'), ('password1', 'password1'), ('password2', 'password2'))
-        context = grpc.metadata_call_context(metadata=metadata)
+    with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
 
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
         response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'))
--- CloudCode/python/virtual_device_service.py
+++ CloudCode/python/virtual_device_service.py
@@ -1,48 +1,17 @@
 from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import threading
+
 from concurrent import futures
-
-from grpc import ServicerContext
-
-from grpc import (
-    ServerInterceptor,
-    ServerCallContext,
-    ServerCredentials,
-    secure_channel_credentials,
-)
 import logging
 
 import grpc
 import iot_service_pb2
 import iot_service_pb2_grpc
 
-class AuthInterceptor(ServerInterceptor):
-    def __init__(self, users):
-        self.users = users
-
-    def intercept_service(self, continuation, handler_call_details):
-        meta = dict(handler_call_details.invocation_metadata)
-        user = meta.get('user', None)
-        password = meta.get('password', None)
-
-        if user and password and self.users.get(user) == password:
-            return continuation(handler_call_details)
-        else:
-            context = handler_call_details.context
-            context.abort(
-                grpc.StatusCode.UNAUTHENTICATED,
-                'Invalid credentials'
-            )
-
-# Dicionário de usuários e senhas
-USERS = {
-    'user1': 'password1',
-    'user2': 'password2',
-}
-
 # Twin state
 current_temperature = 'void'
+current_light_level = 'void'
 led_state = {'red':0, 'green':0}
 
 # Kafka consumer to run on a separate thread
@@ -51,8 +20,17 @@ def consume_temperature():
     consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
     consumer.subscribe(topics=('temperature'))
     for msg in consumer:
-        print (msg.value.decode())
+        print ('Received Temperature: ', msg.value.decode())
         current_temperature = msg.value.decode()
+
+# Kafka consumer to run on a separate thread
+def consume_light_level():
+    global current_light_level
+    consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
+    consumer.subscribe(topics=('lightlevel'))
+    for msg in consumer:
+        print ('Received Light Level: ', msg.value.decode())
+        current_light_level = msg.value.decode()
 
 def produce_led_command(state, ledname):
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
@@ -72,8 +50,11 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
         led_state[request.ledname] = request.state
         return iot_service_pb2.LedReply(ledstate=led_state)
 
+    def SayLightLevel(self, request, context):
+        return iot_service_pb2.LightLevelReply(lightLevel=current_light_level)
+
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), interceptors=[AuthInterceptor(USERS)])
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     iot_service_pb2_grpc.add_IoTServiceServicer_to_server(IoTServer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
@@ -82,8 +63,13 @@ def serve():
 
 if __name__ == '__main__':
     logging.basicConfig()
-    trd = threading.Thread(target=consume_temperature)
-    trd.start()
+
+    trd1 = threading.Thread(target=consume_temperature)
+    trd1.start()
+
+    trd2 = threading.Thread(target=consume_light_level)
+    trd2.start()
+
     # Initialize the state of the leds on the actual device
     for color in led_state.keys():
         produce_led_command (led_state[color], color)
