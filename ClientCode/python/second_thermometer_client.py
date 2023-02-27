c0c87bee6604b0f9e2fc0faf1be5240cbc70929d
Arthur <arthurmb2001@hotmail.com>
2023-02-27 20:56:50 -0300

    Minor fix

--- ClientCode/python/second_thermometer_client.py
+++ ClientCode/python/second_thermometer_client.py
@@ -14,15 +14,15 @@ def get_temperatures(stub):
         temperatures.append(temperature)
     return temperatures
 
-def calculate_mean(temperatures):
+def calculate_avarage(temperatures):
     return sum(temperatures) / len(temperatures)
 
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
         temperatures = get_temperatures(stub)
-        mean_temperature = calculate_mean(temperatures)
-        print("Mean temperature: {:.2f}°C".format(mean_temperature))
+        avarage_temperature = calculate_avarage(temperatures)
+        print("avarage temperature: {:.2f}°C".format(avarage_temperature))
 
 if __name__ == '__main__':
     logging.basicConfig()
