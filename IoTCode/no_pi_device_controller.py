a367c440bd41edf0f68aba63f7f1abf0d42cb940
Arthur <arthurmb2001@hotmail.com>
2023-02-27 19:11:16 -0300

    Novo device controller, sem sensores

---
+++ IoTCode/no_pi_device_controller.py
@@ -0,0 +1,17 @@
+import random
+import time
+from kafka import KafkaProducer
+from const import *
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER+':'+KAFKA_PORT)
+
+while True:
+    # Gerar temperatura aleatória entre 0 e 100 graus Celsius
+    temp_c = random.uniform(0, 100)
+    print('Temperature generated: {:.2f}°C'.format(temp_c))
+
+    # Enviar temperatura para o servidor Kafka
+    producer.send('temperature', str(temp_c).encode())
+
+    # Aguardar um tempo aleatório entre 1 e 5 segundos antes de gerar a próxima temperatura
+    time.sleep(random.uniform(1, 5))
