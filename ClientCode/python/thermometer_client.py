4a98ab2630651297a49d63992807a817e7efa8ed
Arthur <arthurmb2001@hotmail.com>
2023-02-27 23:09:14 -0300

    Criação da autenticação

--- ClientCode/python/thermometer_client.py
+++ ClientCode/python/thermometer_client.py
@@ -20,7 +20,7 @@ def run():
 
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'), metadata)
+        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'), metadata=metadata)
 
     print("Temperatura atual: " + str(round(float(response.temperature), 2)) + "ºC.")
 
