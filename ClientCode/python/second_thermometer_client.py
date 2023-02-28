ffcac29617662bde6a6c55c0db3b710936b2e9b8
Arthur <arthurmb2001@hotmail.com>
2023-02-27 21:07:25 -0300

    Adição de loop

--- ClientCode/python/second_thermometer_client.py
+++ ClientCode/python/second_thermometer_client.py
@@ -10,20 +10,23 @@ from const import *
 
 
 def run():
-    with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
-        stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        temperatures = []
+    while True:
+        with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
+            stub = iot_service_pb2_grpc.IoTServiceStub(channel)
+            temperatures = []
 
-        avarage = 0;
+            avarage = 0;
 
-        for _ in range(5): # recebe as últimas 5 temperaturas
-            response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'))
-            temperature = float(response.temperature)
-            temperatures.append(temperature)
-            avarage = sum(temperatures) / len(temperatures);
+            for _ in range(5): # recebe as últimas 5 temperaturas
+                response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'))
+                temperature = float(response.temperature)
+                temperatures.append(temperature)
+                avarage = sum(temperatures) / len(temperatures);
 
         print("avarage temperature: {:.2f}°C".format(avarage))
 
-if __name__ == '__main__':
-    logging.basicConfig()
-    run()
+        if __name__ == '__main__':
+            logging.basicConfig()
+            run()
+
+    
